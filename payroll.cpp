f55a843452a60215fc9131169e08543bf8ac9bd4
somen2995 <somen2995@gmail.com>
2018-02-21 15:16:01 +0530

    Update payroll.cpp

--- payroll.cpp
+++ payroll.cpp
@@ -9,6 +9,17 @@ struct emp
   float net_salary;
 };
 int main()
-{
+{ struct emp e1[10];
+ strcpy(e1[0].name,"panda");
+ e1[0].emp_id=101;
+ e1[0].age=22;
+ e1[0].gross_salary=1054000;
+ e1[0].net_slary=9500000;
+ strcpy(e1[1].name,"somu");
+ e1[1].emp_id=101;
+ e1[1].age=23;
+ e1[1].gross_salary=7500000;
+ e1[1].net_slary=6500000;
+ 
   return 0;
 }
