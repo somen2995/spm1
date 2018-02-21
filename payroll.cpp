0b33f0bbc2f6358a2fb8bb5fc81a3f3e7bae3813
somen2995 <somen2995@gmail.com>
2018-02-21 15:24:58 +0530

    Update payroll.cpp

--- payroll.cpp
+++ payroll.cpp
@@ -14,12 +14,12 @@ int main()
  e1[0].emp_id=101;
  e1[0].age=22;
  e1[0].gross_salary=1054000;
- e1[0].net_slary=9500000;
+ e1[0].net_salary=9500000;
  strcpy(e1[1].name,"somu");
  e1[1].emp_id=102;
  e1[1].age=23;
  e1[1].gross_salary=7500000;
- e1[1].net_slary=6500000;
+ e1[1].net_salary=6500000;
  cout<<"Enter employee id:";
  int eid;
  cin>>eid;
