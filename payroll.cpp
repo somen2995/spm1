9f886d7734716ced7b4ea923df6a9214615b8058
mohit159 <mohit159.159@gmail.com>
2018-02-21 15:10:14 +0530

    Update payroll.cpp

--- payroll.cpp
+++ payroll.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct emp
+{
+  char name[100];
+  int emp_id;
+  int age;
+  float gross_salary;
+  float net_salary;
+};
 int main()
 {
   return 0;
