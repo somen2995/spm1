c8f76e8be37e230d69fbef148a046798306f3ab3
mohit159 <mohit159.159@gmail.com>
2018-02-21 15:21:41 +0530

    Update payroll.cpp

--- payroll.cpp
+++ payroll.cpp
@@ -16,10 +16,23 @@ int main()
  e1[0].gross_salary=1054000;
  e1[0].net_slary=9500000;
  strcpy(e1[1].name,"somu");
- e1[1].emp_id=101;
+ e1[1].emp_id=102;
  e1[1].age=23;
  e1[1].gross_salary=7500000;
  e1[1].net_slary=6500000;
- 
+ cout<<"Enter employee id:";
+ int eid;
+ cin>>eid;
+ if (eid!=101 && eid!=102)
+   cout<<"Invalid id";
+ else
+ {
+   eid-=101;
+   cout<<"Employee id:"<<e1[eid].emp_id;
+   cout<<"Employee name:"<<e1[eid].name;
+   cout<<"Employee age:"<<e1[eid].age;
+   cout<<"Employee gross salary:"<<e1[eid].gross_salary;
+   cout<<"Employee net salary:"<<e1[eid].net_salary;
+ }
   return 0;
 }
